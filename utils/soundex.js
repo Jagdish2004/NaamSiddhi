b83da463aed9b5064916e3b4ed061249ac64ef9d
Jagdish Raut <jagdishraut2004@gmail.com>
2024-12-12 04:07:13 +0530

    updated indic soundex

--- utils/soundex.js
+++ utils/soundex.js
@@ -1,12 +1,7 @@
-/*
-  Soundex - v0.2.1 - Node.js & Browser
-  Modified for नामसिद्धि project
-*/
-
 function getSoundex(str, scale, mysql) {
     scale = scale || false;
     mysql = mysql || false;
-    
+
     if (!str) return '';
 
     var split = String(str).toUpperCase().replace(/[^A-Z]/g, '').split('');
@@ -18,9 +13,15 @@ function getSoundex(str, scale, mysql) {
         MN: 5,
         R: 6
     };
-    
+
+    var vowels = ['A', 'E', 'I', 'O', 'U'];
     var keys = Object.keys(map).reverse();
-    var build = split.map(function(letter) {
+
+    var build = split.map(function(letter, index) {
+        if (letter === 'W' && index > 0 && vowels.includes(split[index - 1])) {
+            return 1; // Assign key 1 if W comes after a vowel
+        }
+
         for (var num in keys) {
             if (keys[num].indexOf(letter) !== -1) {
                 return map[keys[num]];
@@ -42,11 +43,19 @@ function getSoundex(str, scale, mysql) {
 
     var len = build.length;
     var maxLength = 6;
-    var max = scale ? 
-        Math.min(Math.max(~~((mysql ? len : len * 2 / 3.5)), maxLength), maxLength) : 
-        maxLength;
+    var max = scale
+        ? Math.min(Math.max(~~((mysql ? len : len * 2 / 3.5)), maxLength), maxLength)
+        : maxLength;
 
     var soundexCode = split[0] + build.join('');
+
+    // Replace first letter conditions
+    if (soundexCode[0] === 'E') {
+        soundexCode = 'I' + soundexCode.slice(1);
+    } else if (soundexCode[0] === 'O') {
+        soundexCode = 'U' + soundexCode.slice(1);
+    }
+
     return soundexCode.slice(0, max).replace(/0+$/, '');
 }
 
@@ -57,4 +66,3 @@ if (typeof exports !== 'undefined') {
 } else {
     this['Soundex'] = getSoundex;
 }
-  
